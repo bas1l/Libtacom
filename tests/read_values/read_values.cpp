fb18e968c1e17acb93294a26f8d2108eb0e98b7d
bas1l <basil.duvernoy@gmail.com>
2018-01-31 21:33:37 +0000

    make the daemon taking a place

--- tests/read_values/read_values.cpp
+++ tests/read_values/read_values.cpp
@@ -5,6 +5,15 @@
 #include <stdlib.h>
 #include "GPIOClass.h"
 
+#include <sys/mman.h>
+#include "ad5383.h"
+#include <queue>
+#include <cstdlib>
+#include <chrono>
+#include <ctime>
+#include <random>
+#include <sys/ioctl.h>
+
 using namespace std;
 
 int main (void)
@@ -18,13 +27,58 @@ int main (void)
     
     if (gpio18->setdir_gpio("in") == -1) {return -1;} //GPIO18 set to input
     cout << " Set GPIO pin directions" << endl;
+    
+    std::cout << "Neutral::Begin." << std::endl;
 
+    struct timespec t;
+    struct sched_param param;
+    param.sched_priority = sched_get_priority_max(SCHED_FIFO);
+    
+    /**************** C'EST AVEC CETTE OPTION QUE TOUT BUG *********************
+    if(sched_setscheduler(0, SCHED_FIFO, &param) == -1) {
+            perror("sched_setscheduler failed");
+            exit(-1);
+    }
+    ***************************************************************************/
+    if(mlockall(MCL_CURRENT|MCL_FUTURE) == -1) {
+            perror("mlockall failed");
+            exit(-2);
+    }
+
+    
+    AD5383 ad;
+    ad.spi_open();
+    ad.configure();
+    std::vector<std::vector<uint16_t> > values(AD5383::num_channels);
+    
+    for(int j = 0; j < AD5383::num_channels; ++j)
+    {
+        values[j].push_back(2048);
+    }
+    
+    long ms = 900;
+    
+    
     while(1)
     {
-        usleep(500000);  // wait for 0.5 seconds
+        usleep(50000);  // wait for 0.5 seconds
         gpio18->getval_gpio(inputstate); //read state of GPIO18 input pin
         cout << "Current input pin state is " << inputstate  <<endl;
+        while (inputstate == "0")
+        {
+            gpio18->getval_gpio(inputstate);
+        };
+        std::cout << "Neutral::overruns : " << std::dec << ad.execute_trajectory(values, ms *1000000) << std::endl;
         
+        
+        
+        
+        
+        
+        
+        
+        
+        /*
         if(inputstate == "0") // if input pin is at state "0" i.e. button pressed
         {
             cout << "input pin state is \"Pressed \".n Will check input pin state again in 20ms "<<endl;
@@ -36,19 +90,22 @@ int main (void)
             {
 
                 cout << " Waiting until pin is unpressed....." << endl;
-                while (inputstate == "0")
-                {
-                    gpio18->getval_gpio(inputstate);
-                };
+                
                 cout << "pin is unpressed" << endl;
+                
+   
 
             }
             else
                 cout << "input pin state is definitely \"UnPressed\". That was just noise." <<endl;
 
         }
-
+        */
     }
+    
+    std::cout << "Neutral::Done." << std::endl;
+    ad.spi_close();
+    
     cout << "Exiting....." << endl;
     return 0;
 }
