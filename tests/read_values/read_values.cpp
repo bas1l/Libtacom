28813657db3a31d85212dd2b49459da1d941b677
bas1l <basil.duvernoy@gmail.com>
2018-01-31 22:12:37 +0000

    test only 17

--- tests/read_values/read_values.cpp
+++ tests/read_values/read_values.cpp
@@ -75,7 +75,7 @@ int main (void)
         while (inputstate17 == "0" && inputstate18 == "0")
         {
             gpio17->getval_gpio(inputstate17);
-            gpio18->getval_gpio(inputstate18);
+            //gpio18->getval_gpio(inputstate18);
         };
         
         std::cout << "Power supply : ON" << std::endl;
