6828681af296d7b5d179e7d97d72fc08f22d49d2
bas1l <basil.duvernoy@gmail.com>
2018-01-31 21:25:11 +0000

    is_open == false

--- src/GPIOClass.cpp
+++ src/GPIOClass.cpp
@@ -20,7 +20,8 @@ int GPIOClass::export_gpio()
 {
     string export_str = "/sys/class/gpio/export";
     ofstream exportgpio(export_str.c_str()); // Open "export" file. Convert C++ string to C string. Required for all Linux pathnames
-    if (exportgpio.is_open()){
+    
+    if (exportgpio.is_open() == false){
         cout << " OPERATION FAILED: Unable to export GPIO"<< this->gpionum <<" ."<< endl;
         return -1;
     }
@@ -34,7 +35,8 @@ int GPIOClass::unexport_gpio()
 {
     string unexport_str = "/sys/class/gpio/unexport";
     ofstream unexportgpio(unexport_str.c_str()); //Open unexport file
-    if (unexportgpio.is_open()){
+    
+    if (unexportgpio.is_open() == false){
         cout << " OPERATION FAILED: Unable to unexport GPIO"<< this->gpionum <<" ."<< endl;
         return -1;
     }
@@ -46,42 +48,46 @@ int GPIOClass::unexport_gpio()
 
 int GPIOClass::setdir_gpio(string dir)
 {
-
     string setdir_str ="/sys/class/gpio/gpio" + this->gpionum + "/direction";
     ofstream setdirgpio(setdir_str.c_str()); // open direction file for gpio
-        if (setdirgpio.is_open()){
-            cout << " OPERATION FAILED: Unable to set direction of GPIO"<< this->gpionum <<" ."<< endl;
-            return -1;
-        }
+    
+    if (setdirgpio.is_open() == false){
+        cout << " OPERATION FAILED: Unable to set direction of GPIO"<< this->gpionum <<" ."<< endl;
+        return -1;
+    }
 
-        setdirgpio << dir; //write direction to direction file
-        setdirgpio.close(); // close direction file
-        return 0;
+    setdirgpio << dir; //write direction to direction file
+    setdirgpio.close(); // close direction file
+    
+    return 0;
 }
 
 int GPIOClass::setval_gpio(string val)
 {
-
     string setval_str = "/sys/class/gpio/gpio" + this->gpionum + "/value";
     ofstream setvalgpio(setval_str.c_str()); // open value file for gpio
-        if (setvalgpio.is_open()){
-            cout << " OPERATION FAILED: Unable to set the value of GPIO"<< this->gpionum <<" ."<< endl;
-            return -1;
-        }
+    
+    if (setvalgpio.is_open() == false){
+        cout << " OPERATION FAILED: Unable to set the value of GPIO"<< this->gpionum <<" ."<< endl;
+        return -1;
+    }
 
-        setvalgpio << val ;//write value to value file
-        setvalgpio.close();// close value file
-        return 0;
+    setvalgpio << val ;//write value to value file
+    setvalgpio.close();// close value file
+    
+    return 0;
 }
 
-int GPIOClass::getval_gpio(string& val){
-
+int GPIOClass::getval_gpio(string& val)
+{
     string getval_str = "/sys/class/gpio/gpio" + this->gpionum + "/value";
     ifstream getvalgpio(getval_str.c_str());// open value file for gpio
-    if (getvalgpio.is_open()){
+    
+    if (getvalgpio.is_open() == false)
+    {
         cout << " OPERATION FAILED: Unable to get value of GPIO"<< this->gpionum <<" ."<< endl;
         return -1;
-            }
+    }
 
     getvalgpio >> val ;  //read gpio value
 
@@ -91,6 +97,7 @@ int GPIOClass::getval_gpio(string& val){
         val = "0";
 
     getvalgpio.close(); //close the value file
+    
     return 0;
 }
 
--- tests/read_values/read_values.cpp
+++ tests/read_values/read_values.cpp
@@ -11,38 +11,34 @@ int main (void)
 {
 
     string inputstate;
-    GPIOClass* gpio4 = new GPIOClass("4"); //create new GPIO object to be attached to  GPIO4
-    GPIOClass* gpio17 = new GPIOClass("18"); //create new GPIO object to be attached to  GPIO17
+    GPIOClass* gpio18 = new GPIOClass("18"); //create new GPIO object to be attached to  GPIO18
 
-    gpio4->export_gpio(); //export GPIO4
-    gpio17->export_gpio(); //export GPIO17
-
+    if (gpio18->export_gpio() == - 1) {return -1;} //export GPIO18
     cout << " GPIO pins exported" << endl;
-
-    gpio17->setdir_gpio("in"); //GPIO4 set to output
-    gpio4->setdir_gpio("out"); // GPIO17 set to input
-
+    
+    if (gpio18->setdir_gpio("in") == -1) {return -1;} //GPIO4 set to output
     cout << " Set GPIO pin directions" << endl;
 
     while(1)
     {
         usleep(500000);  // wait for 0.5 seconds
-        gpio17->getval_gpio(inputstate); //read state of GPIO17 input pin
+        gpio18->getval_gpio(inputstate); //read state of GPIO18 input pin
         cout << "Current input pin state is " << inputstate  <<endl;
+        
         if(inputstate == "0") // if input pin is at state "0" i.e. button pressed
         {
             cout << "input pin state is \"Pressed \".n Will check input pin state again in 20ms "<<endl;
-                usleep(20000);
-                    cout << "Checking again ....." << endl;
-                    gpio17->getval_gpio(inputstate); // checking again to ensure that state "0" is due to button press and not noise
+            usleep(20000);
+            cout << "Checking again ....." << endl;
+            gpio18->getval_gpio(inputstate); // checking again to ensure that state "0" is due to button press and not noise
+            
             if(inputstate == "0")
             {
-                cout << "input pin state is definitely \"Pressed\". Turning LED ON" <<endl;
-                gpio4->setval_gpio("1"); // turn LED ON
 
                 cout << " Waiting until pin is unpressed....." << endl;
-                while (inputstate == "0"){
-                gpio17->getval_gpio(inputstate);
+                while (inputstate == "0")
+                {
+                    gpio18->getval_gpio(inputstate);
                 };
                 cout << "pin is unpressed" << endl;
 
