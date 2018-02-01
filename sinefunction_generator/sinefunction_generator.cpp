aa274d765503b5ec8a3c35b7baae350c08e9702e
bas1l <basil.duvernoy@gmail.com>
2018-02-01 15:20:15 +0000

    simplistic

--- sinefunction_generator/sinefunction_generator.cpp
+++ sinefunction_generator/sinefunction_generator.cpp
@@ -557,7 +557,7 @@ std::vector<std::vector<uint16_t> > getvalues(char c, ALPHABET& alph)
         }
         
         default :
-            
+            result = alph.getneutral();
         break;
     }
     
@@ -569,136 +569,6 @@ std::vector<std::vector<uint16_t> > getvalues(char c, ALPHABET& alph)
     
     
     return result;
-}
-
-
-void generateSentences(std::queue<char> & sentences, std::condition_variable & cv,
-            std::mutex & m, std::atomic<bool> & workdone, std::string str_alph)
-{
-
-    initscr();
-    raw();
-    keypad(stdscr, TRUE);
-    noecho();
-    
-    std::string str_ponc = " .,;:!?-'";
-    
-    int ch;
-    printw("You can start to write a letter, a word, a sentence \n --- When you are done, press '*' to Exit ---\n");
-    do{
-        printw("\tStart the do generate sentence\n");
-        if (ch != ERR) 
-        {
-            printw("\tinto the ch!=err\n");
-            // if part of the alphabet
-            if (str_alph.find(ch) != std::string::npos)
-            {
-                printw("%c", ch);
-                printw("\tif\n");
-                
-                std::unique_lock<std::mutex> lk(m);
-                sentences.push(ch);
-                
-                cv.notify_one();
-                cv.wait(lk);
-            }// if part of the ponctuation
-            else if (str_ponc.find(ch) != std::string::npos)
-            {
-                printw("%c", ch);
-                printw("\telseif\n");
-            }
-            else
-            {
-                printw("\telse\n");
-                printw("\n<Key not implemented> Need to Exit ? Press '*'.\n");
-            }
-            
-        }
-    }while((ch = getch()) != '*');
-    
-    
-    printw("\tWHC::create_sentences::End\n");
-    refresh();
-    endwin();
-    workdone = true;
-    cv.notify_one(); // Notify worker
-}
-
-
-
-void workSymbols(std::queue<char> & sentences, std::condition_variable & cv, 
-                std::mutex & m, std::atomic<bool> & workdone, ALPHABET& alph)
-{
-    // init drive electronics
-    AD5383 ad;
-    ad.spi_open();
-    ad.configure();
-    // fréquence maximale pour les sinus utilisées
-    double hz_max = 1000; //Hz=1/s
-    // th. de Nyquist implique :
-    double freq_message = hz_max*2; // 2000 message / secondes (par chan)
-    // un peu bizarre. Mais on souhaite faire 2 envoies de messages par millisec
-    double timePmessage_ns = hz_max/freq_message * ms2ns; // * ns
-    
-    std::vector<std::vector<uint16_t> > values(AD5383::num_channels);
-    values = alph.getneutral();
-    ad.execute_trajectory(values, timePmessage_ns);
-    /*
-    for (int w=0; w<values.size(); ++w)
-    {
-        values[w].clear();
-    }
-    */
-    
-    std::queue<char> letters;
-    
-    // Initialisation complete.
-    cv.notify_one(); // Notify generator (placed here to avoid waiting for the lock)
-    
-    // The goal of this function is to use the letters putted by the other
-    // thread, one by one, and play them consecutively.
-    while(!workdone.load() or !sentences.empty())
-    {
-        
-        printw("Into the while\n");
-        if (!(sentences.empty()))
-        {
-            std::unique_lock<std::mutex> lk(m);
-            
-            if (sentences.empty()) 
-            {
-                printw("Just before the wait\n");
-                cv.wait(lk); // Wait for the generator to complete
-                printw("Just after the wait\n");
-            }
-            if (sentences.empty()) 
-                continue;
-
-            printw("extract the letter\n");
-            // free the common value asap
-            letters.push(sentences.front());
-            sentences.pop();
-            cv.notify_one(); // Notify generator (placed here to avoid waiting for the lock)
-            
-            if (letters.front() != ' ')// is part of the alphabet){
-            {
-                values = getvalues(letters.front(), alph);
-                ad.execute_trajectory(values, timePmessage_ns);
-
-                // for all channels, clear.
-                for (int w=0; w<values.size(); ++w)
-                {
-                    values[w].clear();
-                }
-            }
-            letters.pop();
-        }
-        else
-        {
-            printw("execute the trajectory\n");
-            ad.execute_trajectory(values, timePmessage_ns);
-        }
-     }
 }
 
 
@@ -724,22 +594,60 @@ int main(int argc, char *argv[])
     ALPHABET alph(dev, wf);//, AD5383::num_channels);
     alph.configure();
     
-    std::condition_variable cv;
-    std::mutex m;
-    std::atomic<bool> workdone(false);
     std::queue<char> sentences;
+    // init drive electronics
+    AD5383 ad;
+    ad.spi_open();
+    ad.configure();
+    // fréquence maximale pour les sinus utilisées
+    double hz_max = 1000; //Hz=1/s
+    // th. de Nyquist implique :
+    double freq_message = hz_max*2; // 2000 message / secondes (par chan)
+    // un peu bizarre. Mais on souhaite faire 2 envoies de messages par millisec
+    double timePmessage_ns = hz_max/freq_message * ms2ns; // * ns
     
     
-    std::thread extract_text;
-    extract_text = std::thread(workSymbols, std::ref(sentences), std::ref(cv), 
-                           std::ref(m), std::ref(workdone), std::ref(alph));
-    std::thread send_to_dac;
-    send_to_dac = std::thread(generateSentences, std::ref(sentences), std::ref(cv),
-                           std::ref(m), std::ref(workdone), alph.getlist_alphabet());
+    std::vector<std::vector<uint16_t> > values(AD5383::num_channels);
+    values = alph.getneutral();
+    ad.execute_trajectory(values, timePmessage_ns);
     
-    extract_text.join();
-    send_to_dac.join();
     
+    initscr();
+    raw();
+    keypad(stdscr, TRUE);
+    noecho();
+    
+    int ch;
+    printw("You can start to write a letter, a word, a sentence \n --- When you are done, press '*' to Exit ---\n");
+    do{
+        if (ch != ERR) 
+        {
+            // if c is part of the alphabet
+            if (alph.getlist_alphabet().find(ch) != std::string::npos)
+            {
+                printw("%c", ch);
+                
+                for (int w=0; w<values.size(); ++w)
+                {
+                    values[w].clear();
+                }
+                values = getvalues(ch, alph);
+                
+                ad.execute_trajectory(values, timePmessage_ns);
+            }
+        }
+        else
+        {
+            ad.execute_trajectory(values, timePmessage_ns);
+            printw("execute the trajectory\n");
+        }
+        
+    }while((ch = getch()) != '*');
+    
+    
+    printw("\tWHC::End\n");
+    refresh();
+    endwin();
     
     
     return 0;
