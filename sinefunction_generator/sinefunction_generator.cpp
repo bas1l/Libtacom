8983971d729504f7f0577c8b26634e3821fb968a
bas1l <basil.duvernoy@gmail.com>
2018-02-12 12:28:36 +0000

    same

--- sinefunction_generator/sinefunction_generator.cpp
+++ sinefunction_generator/sinefunction_generator.cpp
@@ -418,8 +418,8 @@ int execute(AD5383& ad, std::vector<std::vector<uint16_t> >& values, long period
         ++value_idx;
         
         high_resolution_clock::time_point t2 = high_resolution_clock::now();
-        double duration = duration_cast<double, std::chrono::nanoseconds>( t2 - t1 ).count();
-        printw("duration=%f, ", duration);
+        double duration = duration_cast<std::chrono::nanoseconds>( t2 - t1 ).count();
+        printw("duration=%f, ", duration.count());
         refresh();
         
         
