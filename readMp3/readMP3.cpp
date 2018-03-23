dedad2e88be8c98c2f48d5dfd2339173a6f7d942
bas1l <basil.duvernoy@gmail.com>
2018-03-23 11:35:41 +0000

    push changes

--- readMp3/readMP3.cpp
+++ readMp3/readMP3.cpp
@@ -8,16 +8,20 @@ int main()
 {
 	string line;
 	//ifstream myfile ("m.mp3");
-        ifstream myfile ("../Libtacom/readMp3/m.mp3", ios::in | ios::binary);
-
+        //ifstream myfile ("../Libtacom/readMp3/m.mp3", ios::in | ios::binary);
+        ifstream myfile ("../Libtacom/sample.au", ios::in | ios::binary);
+        
+        int i = 0;
 	if (myfile.is_open()) {
             while (! myfile.eof() ) {
-              getline (myfile,line);
-              cout << line << endl;
+                
+                getline(myfile,line);
+                cout << line << endl;
+                i++
             }
             
             myfile.close();
-            cout << "Yay!\n";
+            cout << "Yay! i= " << i << endl;
 	}
 	else {
             cout << "Erorr!\n";
