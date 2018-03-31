6ad996fadff476f4718d3e8c1d82ad2ab990b20a
bas1l <basil.duvernoy@gmail.com>
2018-03-31 10:02:46 +0100

    fix error

--- executables/tests/prototype/demo.cpp
+++ executables/tests/prototype/demo.cpp
@@ -165,7 +165,7 @@ void workSymbols(std::queue<char> & sentences, std::condition_variable & cv,
     ad.configure();
     cout << "d..." << endl;
     
-    int durationRefresh_ns = (int) alph->get_freqRefresh_mHz() * ms2ns; // * ns
+    int durationRefresh_ns = (int) alph->get_freqRefresh_mHz() / ms2ns; // * ns
     
     cout << "duration refresh = " << durationRefresh_ns << endl;
     std::vector<std::vector<uint16_t> > values(AD5383::num_channels);
