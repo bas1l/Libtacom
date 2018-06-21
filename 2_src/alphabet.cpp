fc8093ee106ac3d1363d38c2efc6b879e26b2d46
bas1l <basil.duvernoy@gmail.com>
2018-06-21 13:07:27 +0200

    as

--- 2_src/alphabet.cpp
+++ 2_src/alphabet.cpp
@@ -14,12 +14,12 @@ using namespace std;
 
 
 ALPHABET::ALPHABET() : 
-        listSymbols("abcdefghijklmnopqrstuvwxyz~+-12 ") {}
+        listSymbols("abcdefghijklmnopqrstuvwxyz~+-1234567890 ") {}
 
 ALPHABET::ALPHABET(DEVICE * _dev, WAVEFORM * _wf) :
         dev(_dev), 
         wf( _wf),
-        listSymbols("abcdefghijklmnopqrstuvwxyz~+-12 ") {}
+        listSymbols("abcdefghijklmnopqrstuvwxyz~+-1234567890 ") {}
 
 ALPHABET::~ALPHABET() {}
 
